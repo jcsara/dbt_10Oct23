a9645121b4f21d44cbaf8cce95b218645da644f7
Saravanan Shanmugam <jcsara.dbt@gmail.com>
2023-10-13 04:50:55 +0000

    rename

--- analyses/full_moon_reviews_sentiment.sql
+++ analyses/full_moon_reviews_sentiment.sql
@@ -1,11 +1,11 @@
-WITH mart_fullmoon_reviews AS ( 
+WITH analyses_full_moon_reviews_sentiment AS ( 
     SELECT * FROM {{ ref('full_moon_reviews') }} )
 SELECT
  is_full_moon,
  review_sentiment,
  COUNT(*) as reviews
 FROM
- mart_fullmoon_reviews
+ analyses_full_moon_reviews_sentiment
  GROUP BY
  is_full_moon,
  review_sentiment
